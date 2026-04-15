e688c11910d165f5f2dcd515c68313089552e770
Wasim30596 <wasim3096k@gmail.com>
2026-04-15 11:21:19 +0530

    dev folders are pushed to dev branch

---
+++ environments/dev/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.40.0"
+    }
+  }
+  backend "s3" {
+    bucket = "my-terraform-state-bucket-1504"
+    key    = "environments/dev/terraform.tfstate"
+    region = "ap-south-1"
+    Dynamotable_table = "terraform-lock-table"
+  }
+}
+    
+provider "aws" {
+  region = var.aws_region
+}
---
+++ environments/dev/main.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+  source = "../../modules/vpc"
+  vpc_cidr_block = "10.0.0.0/16"
+  subnet_cidr_block = "10.0.0.0/24"
+  availability_zone = "${var.aws_region}a"
+  env = "dev"
+}
+
+module "ec2" {
+  source = "../../modules/ec2"
+  instance_type = "t3.small"
+  ami_id = "ami-05d2d839d4f73aafb" # ubuntu AMI
+  ec2_count = 1
+  subnet_id = module.vpc.subnetid
+  env = "dev"
+}
